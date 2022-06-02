640bb7a7cf775ef0a9c371d922f6f13f67b1c754
Gabe Joseph <gjoseph92@gmail.com>
2022-06-02 17:10:15 -0600

    update build script and build all versions

--- senv/make-coiled-envs.py
+++ senv/make-coiled-envs.py
@@ -1,17 +1,21 @@
 """
-Build lots of Coiled software environments for every version of Python.
+Build lots of Docker images and Coiled software environments for every version of Python.
 
-These are extremely lightweight for fast cluster startup, and just contain Python and Poetry.
+These are lightweight---they're just the official `slim` Python docker images with Poetry installed
+and a tiny script added. When the image launches, it installs more pip packages (dask, distributed, bokeh)
+via an environment variable, then runs the dask command requested.
 
-FIXME though public, they can't actually be run by other v2 users!
-This whole strategy won't work right now.
-We could use public docker images, but dockerhub has ratelimiting.
+This script builds the images locally,
+
+TODO though public, can these actually be run by other v2 users?
+Since the images are hosted on dockerhub, I'm hoping so, though I'm worried about ratelimiting.
 """
 
 from __future__ import annotations
 
 import asyncio
 import io
+import os
 import tarfile
 import traceback
 from contextlib import contextmanager
@@ -23,32 +27,32 @@ from coiled.core import Async, Cloud
 from rich import print
 
 PY_VERSIONS = [
-    # "3.10.4",
-    # "3.10.2",
-    # "3.10.1",
-    # "3.10.0",
-    # "3.9.13",
-    # "3.9.12",
-    # "3.9.10",
-    # "3.9.9",
-    # "3.9.7",
-    # "3.9.6",
-    # "3.9.5",
+    "3.10.4",
+    "3.10.2",
+    "3.10.1",
+    "3.10.0",
+    "3.9.13",
+    "3.9.12",
+    "3.9.10",
+    "3.9.9",
+    "3.9.7",
+    "3.9.6",
+    "3.9.5",
     "3.9.4",
     "3.9.2",
     "3.9.1",
     "3.9.0",
-    # "3.8.13",
-    # "3.8.12",
-    # "3.8.10",
-    # "3.8.8",
-    # "3.8.6",
-    # "3.8.5",
-    # "3.8.4",
-    # "3.8.3",
-    # "3.8.2",
-    # "3.8.1",
-    # "3.8.0",
+    "3.8.13",
+    "3.8.12",
+    "3.8.10",
+    "3.8.8",
+    "3.8.6",
+    "3.8.5",
+    "3.8.4",
+    "3.8.3",
+    "3.8.2",
+    "3.8.1",
+    "3.8.0",
 ]
 
 DOCKER_USERNAME = "jabriel"
@@ -100,6 +104,7 @@ async def make_image(docker: Docker, tarpath: Path, py_version: str) -> str:
     except Exception as exc:
         print(f"[bold red]Error with image {name}: {exc}")
         traceback.print_exc()
+        raise
 
     print(f"[bold blue]Image {name} complete[/]")
     return name
@@ -134,7 +139,7 @@ async def make_envs(
     tarpath: Path,
     versions: list[str],
     *,
-    concurrency: int = 6,
+    coiled_concurrency: int = 6,
 ) -> None:
     docker_builds: asyncio.Queue[str] = asyncio.Queue()
     senv_creates: asyncio.Queue[str] = asyncio.Queue()
@@ -142,18 +147,26 @@ async def make_envs(
     async def _docker():
         while True:
             version = await docker_builds.get()
-            image = await make_image(docker, tarpath, version)
-            await senv_creates.put(image)
-            docker_builds.task_done()
+            try:
+                image = await make_image(docker, tarpath, version)
+                await senv_creates.put(image)
+            except Exception:
+                pass
+            finally:
+                docker_builds.task_done()
 
     async def _senv():
         while True:
             image = await senv_creates.get()
-            await make_env(cloud, image)
-            senv_creates.task_done()
+            try:
+                await make_env(cloud, image)
+            except Exception:
+                pass
+            finally:
+                senv_creates.task_done()
 
-    tasks = [asyncio.create_task(_docker()) for _ in range(8)]  # TODO ncores
-    tasks += [asyncio.create_task(_senv()) for _ in range(concurrency)]
+    tasks = [asyncio.create_task(_docker()) for _ in range(os.cpu_count() or 2)]
+    tasks += [asyncio.create_task(_senv()) for _ in range(coiled_concurrency)]
 
     for v in versions:
         docker_builds.put_nowait(v)
