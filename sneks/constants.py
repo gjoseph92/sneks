98f522977ca2b0407b8ef606eff25780da1cde67
Gabe Joseph <gjoseph92@gmail.com>
2022-09-20 18:39:47 -0600

    add pyarrow to optionals
    this is probably unnecessary (it isn't needed for initial connection).

--- sneks/constants.py
+++ sneks/constants.py
@@ -4,4 +4,4 @@ PROJECT_NAME = "sneks"
 REQUIRED_PACKAGES = frozenset(
     ["dask", "distributed", "bokeh", "cloudpickle", "msgpack"]
 )
-OPTIONAL_PACKAGES = frozenset(["numpy", "pandas", "s3fs"])
+OPTIONAL_PACKAGES = frozenset(["numpy", "pandas", "s3fs", "pyarrow"])
