3309bb56830b976292452541c4da853126835d33
Gabe Joseph <gjoseph92@gmail.com>
2022-11-18 13:35:00 -0700

    more meaningful suffix

--- sneks/constants.py
+++ sneks/constants.py
@@ -1,7 +1,7 @@
 COILED_ACCOUNT_NAME = "gjoseph92"
 DOCKER_USERNAME = "jabriel"
 PROJECT_NAME = "sneks"
-SUFFIX = "-full-multi"
+SUFFIX = "-full-xarch"
 REQUIRED_PACKAGES = frozenset(
     ["dask", "distributed", "bokeh", "cloudpickle", "msgpack"]
 )
