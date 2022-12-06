28f0b0919ee9b0e202e60d817cd77d347df8c013
Gabe Joseph <gjoseph92@gmail.com>
2022-12-06 15:22:39 -0700

    add mypy_extensions override for testing

--- sneks/tests/update_test_envs.py
+++ sneks/tests/update_test_envs.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 """
 This keeps the fixture lockfiles used in tests up to date, so their distributed, cloudpickle, etc.
 versions match what will be running on the client. Otherwise, adding the DepManager plugin using
@@ -29,13 +31,25 @@ def update_poetry_env(env: Path, versions: list[str]) -> None:
     )
 
 
-def update_pdm_env(env: Path, versions: list[str]) -> None:
+def update_pdm_env(env: Path, versions: list[str], overrides: dict[str, str]) -> None:
     env.mkdir(exist_ok=True)
     if not (env / "pyproject.toml").exists():
         subprocess.run(["pdm", "init", "-n"], shell=False, check=True, cwd=env)
     subprocess.run(
         # --no-sync prevents actually installing them; just does lock
         ["pdm", "add", "--no-sync"] + versions,
+        shell=False,
+        check=True,
+        cwd=env,
+    )
+
+    with open(env / "pyproject.toml", "a") as f:
+        f.write("\n[tool.pdm.overrides]\n")
+        for k, v in overrides.items():
+            f.write(f'{k} = "{v}"\n')
+
+    subprocess.run(
+        ["pdm", "lock"],
         shell=False,
         check=True,
         cwd=env,
@@ -53,7 +67,15 @@ def update_test_envs():
     print("[bold][white]Setting up Poetry env[/]")
     update_poetry_env(tests / "env-for-running-poetry", versions)
     print("[bold][white]Setting up PDM env[/]")
-    update_pdm_env(tests / "env-for-running-pdm", versions)
+    update_pdm_env(
+        tests / "env-for-running-pdm",
+        versions,
+        overrides={
+            # Override a transitive dependency of `black`:
+            # https://github.com/psf/black/blob/d4a85643a465f5fae2113d07d22d021d4af4795a/pyproject.toml#L67
+            "mypy_extensions": "0.4.2"
+        },
+    )
 
 
 if __name__ == "__main__":
